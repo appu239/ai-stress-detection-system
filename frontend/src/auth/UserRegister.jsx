aa3c73b182facc194f56f2323cbcedfdabe3f2ca
Aparna C <aparnac239@gmail.com>
2026-06-08 00:19:49 +0530

    FINAL FIX REGISTER WORKING

--- frontend/src/auth/UserRegister.jsx
+++ frontend/src/auth/UserRegister.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import PublicFooter from "../components/PublicFooter";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { getApiUrl } from "../utils/api";
