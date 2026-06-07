3b56a2f23dd62fc0f338b81e98dc3f48583f06e5
Aparna C <aparnac239@gmail.com>
2026-06-07 22:25:32 +0530

    added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://multimodal-stress-detection-4.onrender.com",
+  baseURL: "https://stress-backend-w7nb.onrender.com",
 });
 
 export const analyzeAudio = (formData) =>
   API.post("/predict", formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
-
-export const analyzeText = (text) =>
-  API.post("/predict_text", { text });
 
 export default API;
